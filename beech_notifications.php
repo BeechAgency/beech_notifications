65d5919668c60928aca82edf7ec3a9bda590a8da
joshwayman <nowayman121@gmail.com>
2024-04-04 14:25:43 +1100

    Updated the version
    1.2 BABY!

--- beech_notifications.php
+++ beech_notifications.php
@@ -37,7 +37,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'BEECH_NOTIFICATIONS_VERSION', '1.1' );
+define( 'BEECH_NOTIFICATIONS_VERSION', '1.2' );
 
 /**
  * The code that runs during plugin activation.
